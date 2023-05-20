664f4c5d4eea56d1bd5e5d75a2f8873e52a03999
MohamedHussien199 <115949673+MohamedHussien199@users.noreply.github.com>
2023-05-20 11:47:19 +0200

    Add files via upload

--- des.m
+++ des.m
@@ -1,21 +1,23 @@
 function [x numiter]=des(A,b,h,tol)
-n=length(b);
-xprev=zeros(1,n)';
-w=h*A'*A;
-w1=h*A'*b;
-N=eye(n)-w;
-iter=0;
+n=length(b);% size of b.
+xprev=zeros(1,n)';% the initial guess.
+w=h*A'*A; % to construct the iteration matrix.
+w1=h*A'*b;% the iteration vector.
+N=eye(n)-w; % the iteration matrix.
+iter=0; % to count the number of iterations.
 while true
     iter=iter+1;
     xnew=(N*xprev)+w1;
+    % stopping criteria.
     if norm(xnew-xprev,2)<tol
         numiter=iter;
-        x=xnew
+        x=xnew;
         break
     end
     xprev=xnew;
 end
-if norm(xnew-xprev)>tol
+% in case of divergence.
+if norm(xnew-xprev)>=tol
     numiter=-1;
     x=xnew;
 end
